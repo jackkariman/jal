01fffe3902f22d5ff093c27b92f3c4e0ff6e8fda
jackkariman <52946341+jackkariman@users.noreply.github.com>
2020-01-12 22:52:24 +0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,45 @@
+<style>
+table, th, td {
+  border: 1px solid white;
+  padding:5px 10px;
+}
+tr {
+  font:normal 14px Arial, Helvetica, sans-serif;
+  -webkit-transition:All 1s ease;
+  -moz-transition:All 1s ease;
+  -o-transition:All 1s ease;
+  transition:All 1s ease;
+}
+}
+}
+th {
+  background-image: linear-gradient(bottom, #5789AB 13%, #96C4D9 67%);
+  background-image: -o-linear-gradient(bottom, #5789AB 13%, #96C4D9 67%);
+  background-image: -moz-linear-gradient(bottom, #5789AB 13%, #96C4D9 67%);
+  background-image: -webkit-linear-gradient(bottom, #5789AB 13%, #96C4D9 67%);
+  background-image: -ms-linear-gradient(bottom, #5789AB 13%, #96C4D9 67%);
+  color:white;
+  font:normal 16px Impact, Charcoal, sans-serif;
+  text-shadow: 1px 1px 0px #151F26;
+}
+.col1 {background:#FFFFFF;}
+.col2 {background:#F5F5F5;}
+
+thead
+{
+    background-color:#FF00FF;
+    text-align: center;   
+    color: white;
+}
+
+td
+{
+ text-align: center;
+}
+
+tbody tr:nth-child(even) 
+{
+    background-color:#FFF0F5;
+}
+
+</style>
